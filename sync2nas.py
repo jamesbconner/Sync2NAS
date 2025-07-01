9347e3175ec96ab4187bf788cf9cba1cf2c239d3
James <jamesbconner@users.noreply.github.com>
2025-07-01 18:59:49 -0400

    Updated TODO list

--- sync2nas.py
+++ sync2nas.py
@@ -14,11 +14,10 @@ from cli.main import sync2nas_cli
 # TODO: Check for new seasons of shows existing in the inventory on AniDb (periodic pull of AniDB)
 # TODO: Add a de-deupe function to identify duplicate show/episodes in the inventory for pruning
 # TODO: Rework special character handling in show names (primary and aliases)
-# TODO: Add a function to check for and handle show/episode renames and updates to the database
 # TODO: Add IMDB, TVDB and AniDB APIs as optional sources for show information if TMDB info missing
 # TODO: Better checks for handling specials and OVA's 
 # TODO: Add genre, language and other identifiers to the search and fix-show functions
-# TODO: MCP LLM integration for show and episode filename parsing ... why regex it when you can ask the LLM?
+# Done: MCP LLM integration for show and episode filename parsing ... why regex it when you can ask the LLM?
 # TODO: MCP Server integration with TMDB, AniDB, TVDB, IMDB, etc. to get show and episode information
 # TODO: MCP Server integration with database backend to update/add shows, episodes, etc. Already supports SQLite
 # TODO: Try vector DB for similarity search and recommendations ... Milvus, Chroma, Qdrant, Weaviate, Faiss, etc.
@@ -26,6 +25,15 @@ from cli.main import sync2nas_cli
 # TODO: MCP Server RSS Feed integration for new show notifications
 # TODO: Add discord integration for notifications of scene releases
 # TODO: Implement Anthropic LLM support in the LLM factory and as a service implementation
+# TODO: Add a CRC32 hash check to the file routing function to confirm file integrity
+# TODO: Add LLM checks to API health checks
+# TODO: Improve the prompt engineering for OpenAI models
+# TODO: Add test coverage for the LLMs & factory
+# TODO: Add test coverage for database factory
+# TODO: Add show name to sys_name logic for LLM when using --auto-add and --auto-fix flags
+# TODO: Move some of the file routing logic from the utils/, since it's not really a common function
+# TODO: Add more comments to the codebase to improve readability
+# TODO: Generalize the application away from hardcoding variable name of anime_tv in the ctx.obj. Consider using the "Routing" keys in the config
 
 
 if __name__ == '__main__':
